5948f2da95ed243f2641a4e676ced8542aa613d3
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-07-29 00:21:58 -0300

    fix:

--- tesy.sh
+++ tesy.sh
@@ -6,19 +6,19 @@ local randomTxt4 = $RANDOM
 
 echo $randomTxt1 > test1.txt
 git add .
-git ci fix: $randomTxt1
+git ci "fix: $randomTxt1"
 
 echo $randomTxt1 > test2.txt
 git add .
-git ci test: $randomTxt2
+git ci "test: $randomTxt2"
 
 echo $randomTxt1 > test3.txt
 git add .
-git ci chore: $randomTxt3
+git ci "chore: $randomTxt3"
 
 echo $randomTxt1 > test4.txt
 git add .
-git ci feat: $randomTxt4
+git ci "feat: $randomTxt4"
 
 rm -rf test*
 git add .
