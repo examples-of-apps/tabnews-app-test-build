dbc0768f337e73754a5f4b2b5d2e9eb61aa7db56
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-07-29 00:26:10 -0300

    fix: randomTxt1

--- tesy.sh
+++ tesy.sh
@@ -1,23 +1,18 @@
-randomTxt1 = $RANDOM
-randomTxt2 = $RANDOM
-randomTxt3 = $RANDOM
-randomTxt4 = $RANDOM
-
-echo $randomTxt1 > test1.txt
+echo $RANDOM > test1.txt
 git add .
-git ci fix: $randomTxt1
+git ci fix: randomTxt1
 
-echo $randomTxt1 > test2.txt
+echo $RANDOM > test2.txt
 git add .
-git ci test: $randomTxt2
+git ci test: randomTxt2
 
-echo $randomTxt1 > test3.txt
+echo $RANDOM > test3.txt
 git add .
-git ci chore: $randomTxt3
+git ci chore: randomTxt3
 
-echo $randomTxt1 > test4.txt
+echo $RANDOM > test4.txt
 git add .
-git ci feat: $randomTxt4
+git ci feat: randomTxt4
 
 rm -rf test*
 git add .
