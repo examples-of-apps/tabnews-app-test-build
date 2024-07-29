d45ecd307835d0c304eaed53265f886ce1b8b114
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-07-29 00:15:55 -0300

    commit

--- tesy.sh
+++ tesy.sh
@@ -3,21 +3,21 @@ randomTxt2 = $RANDOM
 randomTxt3 = $RANDOM
 randomTxt4 = $RANDOM
 
-echo randomTxt1 > test1.txt
+echo $randomTxt1 > test1.txt
 git add .
-git ci fix: randomTxt1
+git ci fix: $randomTxt1
 
-echo randomTxt1 > test2.txt
+echo $randomTxt1 > test2.txt
 git add .
-git ci test: randomTxt2
+git ci test: $randomTxt2
 
-echo randomTxt1 > test3.txt
+echo $randomTxt1 > test3.txt
 git add .
-git ci chore: randomTxt3
+git ci chore: $randomTxt3
 
-echo randomTxt1 > test4.txt
+echo $randomTxt1 > test4.txt
 git add .
-git ci feat: randomTxt4
+git ci feat: $randomTxt4
 
 rm -rf test*
 git add .
