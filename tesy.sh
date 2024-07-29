d880b6ff6acaf8ff9144e9775cbcd148514aaee8
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-07-29 00:06:00 -0300

    fix: randomTxt1

---
+++ tesy.sh
@@ -0,0 +1,24 @@
+randomTxt1 = $RANDOM
+randomTxt2 = $RANDOM
+randomTxt3 = $RANDOM
+randomTxt4 = $RANDOM
+
+echo randomTxt1 > test1.txt
+git add .
+git ci fix: randomTxt1
+
+echo randomTxt1 > test2.txt
+git add .
+git ci test: randomTxt2
+
+echo randomTxt1 > test3.txt
+git add .
+git ci chore: randomTxt3
+
+echo randomTxt1 > test4.txt
+git add .
+git ci feat: randomTxt4
+
+rm -rf test*
+git add .
+git ci fix: remove files
